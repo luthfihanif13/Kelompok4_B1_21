34ef71fd21bbc7a092de5798daabfbb9d74929fe
zahrasalsabila12 <zah12asalsabila@gmail.com>
2022-11-09 21:37:22 +0800

    register

--- register.php
+++ register.php
@@ -1,3 +1,37 @@
+<?php 
+include 'koneksi.php';
+if(isset($_POST['register'])){
+    $username = $_POST['nama'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['confirm-pass'];
+
+    if($password === $cpassword){
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $result = mysqli_query($conn, "SELECT * FROM akun WHERE nama = '$username'");
+        if(mysqli_fetch_assoc($result)){
+            echo "<script>
+            alert('Username Sudah digunakan');
+            document.location.href = 'register.php';
+            </script>";
+        }else {
+            $sql = "INSERT INTO akun (nama, password) VALUES ('$username', '$password')";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_affected_rows($conn) > 0) {
+                echo "<script>
+                alert('Registrasi Telah Berhasil');
+                document.location.href ='login_user.php';
+                </script>";
+            } else{
+                echo "<script>
+                alert('Registrasi Gagal!!!');
+                document.location.href ='login_user.php';
+                </script>";
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,10 +49,10 @@
            <h1>REGISTRASI</h1>
        </div>
        <div class="login-area">
-           <input type="text" class="username" placeholder="Username" required>
-           <input type="password" class="password" placeholder="Password" required>
-           <input type="password" class="password" type="password" placeholder="Konfirmasi Password" required>
-           <input type="submit" value="registrasi" class="submit">
+           <input type="text" name="username" id="username" class="username" placeholder="Username" required>
+           <input type="password" name="password" id="password" class="password" placeholder="Password" required>
+           <input type="password"  name="confirm-pass" id="confirm-pass" class="password" type="password" placeholder="Konfirmasi Password" required>
+           <input type="submit" class="submit" name="register" id="register">
            <a href="login_user.php">Sudah punya akun? Login</a>
        </div>
    </form> 
