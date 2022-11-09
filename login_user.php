aa8dcbc854bca66de213744bb8e1a4a8bfe7c12a
zahrasalsabila12 <zah12asalsabila@gmail.com>
2022-11-09 21:13:39 +0800

    login_user

--- login_user.php
+++ login_user.php
@@ -1,3 +1,23 @@
+<?php
+    include 'koneksi.php';
+
+    if(isset($_POST['login_user'])){
+        $username = $_POST['nama'];
+        $password = $_POST['password'];
+
+        $result = mysqli_query($connect, "SELECT * FROM akun WHERE nama ='$username'");
+        if(mysqli_num_rows($result) == 1){
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($password, $row['password'])){
+                $_SESSION['login_user'] = true;
+                header("location:index.php");
+                exit;
+            }
+        }
+    $error = true;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +34,15 @@
        <div class="header_user">
            <h1>LOGIN USER</h1>
        </div>
+       <?php if(isset($error)){
+            echo '<script type="text/javascript">
+                    window.onload = function () { alert("Username atau Password Salah!"); } 
+                </script>'; 
+        }?>
        <div class="login-area">
-           <input type="text" class="username" placeholder="Username">
-           <input type="password" class="password" placeholder="Password">
-           <input type="submit" value="Login" class="submit">
+           <input type="text" class="username" placeholder="Username" name="username" id="username">
+           <input type="password" class="password" placeholder="Password" type="password" name="password">
+           <input type="submit" name="login_user" class="submit">
            <a href="login_admin.php">Beralih ke Admin?</a>
            <a href="register.php">Belum punya akun? Register</a>
        </div>
