cfb890444a2541e593650a41160099a5b1547486
zahrasalsabila12 <zah12asalsabila@gmail.com>
2022-11-09 22:26:24 +0800

    login_user

--- login_user.php
+++ login_user.php
@@ -5,7 +5,7 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $result = mysqli_query($conn, "SELECT * FROM akun WHERE username ='$username'");
+        $result = mysqli_query($conn, "SELECT * FROM 'akun' WHERE username ='$username'");
         if(mysqli_num_rows($result) == 1){
             $row = mysqli_fetch_assoc($result);
             if(password_verify($password, $row['password'])){
