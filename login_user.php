42adf61749f55b27f7d9e832340bf51420eed25f
zahrasalsabila12 <zah12asalsabila@gmail.com>
2022-11-09 22:21:40 +0800

    login_user

--- login_user.php
+++ login_user.php
@@ -2,10 +2,10 @@
     include 'koneksi.php';
 
     if(isset($_POST['login'])){
-        $username = $_POST['nama'];
+        $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $result = mysqli_query($conn, "SELECT * FROM akun WHERE nama ='$username'");
+        $result = mysqli_query($conn, "SELECT * FROM akun WHERE username ='$username'");
         if(mysqli_num_rows($result) == 1){
             $row = mysqli_fetch_assoc($result);
             if(password_verify($password, $row['password'])){
