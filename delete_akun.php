5fcbca3ad74ec8ec1dfe31918ec073c1cae48c37
zahrasalsabila12 <zah12asalsabila@gmail.com>
2022-11-10 12:21:58 +0800

    delete_akun

--- delete_akun.php
+++ delete_akun.php
@@ -2,7 +2,7 @@
     include "../koneksi.php";
 
     $idAkun = $_GET['id'];
-    $query = mysqli_query($conn, "DELETE FROM detailproduk WHERE id='$idAkun'");
+    $query = mysqli_query($conn, "DELETE FROM akun WHERE id='$idAkun'");
         
         if($query){
             echo"Data Berhasil di Delete";
