9ccf2199fe81a100e685e60b533f17c107a1a7da
zahrasalsabila12 <zah12asalsabila@gmail.com>
2022-11-14 09:23:03 +0800

    Lihat request

--- admin_lihat_request.php
+++ admin_lihat_request.php
@@ -13,34 +13,32 @@ while ($row = mysqli_fetch_assoc($result)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/LogoZhongliB.ico">
-    <title>Request komik</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style_admin.css">
+  <link rel="shortcut icon" href="images/LogoZhongliB.ico">
+  <title>Menu Admin</title>
     <style>
-    body {
-      width: 100%;
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: #588d24;
-    }
-
     .container {
-      width: 60%;
+      width: 95%;
       height: 60vh;
       display: flex;
       flex-direction: column;
       justify-content: center;
-      background: rgb(117, 159, 26);
       border-radius: 10px;
+      margin-top: 25px;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 25px;
+      align-items: center;
+      background: rgb(117, 159, 26);
     }
 
     .container h1 {
       color: black;
       text-align: center;
+      margin-bottom: 25px;
     }
 
     table {
@@ -48,6 +46,7 @@ while ($row = mysqli_fetch_assoc($result)) {
       border-collapse: collapse;
       background: rgb(107, 202, 81);
       color: #000000;
+      margin-bottom: auto;
     }
 
     th, td {
@@ -55,9 +54,35 @@ while ($row = mysqli_fetch_assoc($result)) {
       border: 2px solid #000000;
       padding: 20px 30px;
     }
+
+    .del-button{
+      background-color: #FF0000;
+      color: white;
+      padding: 5px 5px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      text-decoration: none;
+    }
     </style>
 </head>
 <body>
+  <header>
+    <a href="#"><h1><img src="images/Zhongli.png" width="140px" height="120px"></h1></a>
+    <nav>
+      <a> Cari Komik <input type="text"></a>
+      <a href="admin_menu.php">Home</a>
+      <a href="admin_lihat_request.php">Lihat Request Komik</a>
+      <a href="admin_lihat_akun_user.php">Lihat Akun</a>
+      <a href="admin_login.php" class="me">Log Out</a>
+    </nav>
+  </header>
+  <div class="mode">
+    <p id="change-mode">Dark Mode</p>
+    <img id="icon-mode" src="images/moon.png" alt="">
+    <input type="checkbox" onclick="lightMode()">
+    <br>
+  </div>
     <div class="container">
     <h1>Request komik</h1>
     <table border=1px>
@@ -82,11 +107,21 @@ while ($row = mysqli_fetch_assoc($result)) {
             <td><?php echo $req["tanggal_rilis"] ;?></td>
             <td><?php echo $req["sinopsis"] ;?></td>
             <td><?php echo $req["waktu_input"] ;?></td>
-            <td><a href="proses_hapus_request.php?id=<?php echo $req["id"]; ?>" onclick = "return confirm('Anda yakin ingin mengahpus data ini ?')">Hapus</a></td>
+            <td><a href="proses_hapus_request.php?id=<?php echo $req["id"]; ?>" onclick = "return confirm('Anda yakin ingin mengahpus data ini ?')" class="del-button">Hapus</a></td>
         </tr>
         <?php $i++; endforeach;?>
     </table>
     </div>
-    
+    <footer>
+    <nav>
+      <br>
+      <a href="https://youtube.com/channel/UCjRc-gYlqHi1x9fwCMeVEGw">Youtube</a>
+      <a href="https://instagram.com/mirvanhakimmm">Instagram</a>
+      <a href="https://www.tiktok.com/@muhammad_irvan_hakim?_t=8VzzYA3N1qj&_r=1">TikTok</a>
+    </nav>
+    <p>&copy Copyright 2022 - by Kelompok 4 </p>
+  </footer>
+
+  <script src="script.js"></script>
 </body>
 </html>
